0f231dffcdf112e719780f55154b9668bdf5f6b2
lendelpe <georgelendelpe03@gmail.com>
2024-09-02 22:10:40 +0800

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,12 +36,64 @@ const bgHeader = () => {
 		: header.classList.remove("bg-header");
 };
 window.addEventListener("scroll", bgHeader);
-bgHeader()
+bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
+const swiperServices = new Swiper(".services__swiper", {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 24,
+	slidesperPerView: "auto",
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+const scrollUp = document.getElementById('scroll-up')
+// When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+scrollUp()
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+const scrollDown = window.scrollY
+
+
+sections.forEach(current =>{
+	const sectionHeight = current.offsetHeight,
+		  sectionTop = current.offsetTop - 58,
+		  sectionId = current.getAttribute('id'),
+		  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+	if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+		sectionsClass.classList.add('active-link')
+	}else{
+		sectionsClass.classList.remove('active-link')
+	}
+})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 2500,
+  delay: 400,
+  reset: true // Animation repeat
+})
+
+sr.reveal('.home__content, .services__data, .services__swiper, .footer__container')
+sr.reveal('.home__images', {origin: 'bottom', delay: 1000})
+sr.reveal('.about__images, .contact__image', {origin: 'left'})
+sr.reveal('.about__data, .contact__data', {origin: 'right'})
+sr.reveal('.projects__card', {interval: 100})
